92828c30b76aa774c8d848290c5c2d3ef3ca2ce0
Avirup889 <91195102+Avirup889@users.noreply.github.com>
2021-09-25 23:42:27 +0530

    Add files via upload
    python for data science Assignment 2

---
+++ project 2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+lst= [1,2,3,4,5,6,7,8,9]
+
+
+# In[5]:
+
+
+for num in lst :
+    if num %2==0:
+        print (num,end =" ")
+
+
+# In[ ]:
+
+
+
+
