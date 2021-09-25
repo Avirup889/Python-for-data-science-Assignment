2873c2854dbb6bef1e4d617180e4e4ca7a9ad161
Avirup889 <91195102+Avirup889@users.noreply.github.com>
2021-09-25 23:35:43 +0530

    Add files via upload
    Python for data science Assignment1 submitted

---
+++ project1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+principal=3000
+
+
+# In[2]:
+
+
+rate = 5
+
+
+# In[3]:
+
+
+time =3
+
+
+# In[4]:
+
+
+SI= principal*rate*time/100
+
+
+# In[5]:
+
+
+print ("SI =",SI)
+
+
+# In[ ]:
+
+
+
+
